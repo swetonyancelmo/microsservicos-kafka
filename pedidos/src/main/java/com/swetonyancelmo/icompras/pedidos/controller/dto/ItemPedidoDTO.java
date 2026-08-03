e3790fbffe7e11676041efb8255bf334cd5741d1
Swetony Ancelmo <swetonyancelmo@gmail.com>
2026-08-03 20:49:09 -0300

    fix: corrige bug no pedido

--- pedidos/src/main/java/com/swetonyancelmo/icompras/pedidos/controller/dto/ItemPedidoDTO.java
+++ pedidos/src/main/java/com/swetonyancelmo/icompras/pedidos/controller/dto/ItemPedidoDTO.java
@@ -3,6 +3,7 @@ package com.swetonyancelmo.icompras.pedidos.controller.dto;
 import java.math.BigDecimal;
 
 public record ItemPedidoDTO(
+        Long codigoProduto,
         Long codigoPedido,
         Integer quantidade,
         BigDecimal valorUnitario
